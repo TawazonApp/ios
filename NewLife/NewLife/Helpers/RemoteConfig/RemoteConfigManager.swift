452047e9addcdfa56e7909b57d9ee0a196b4a98c
Rawan <eng.rawan.saeed@gmail.com>
2022-03-16 08:48:04 +0200

    po remoteConfig

---
+++ NewLife/NewLife/Helpers/RemoteConfig/RemoteConfigManager.swift
@@ -0,0 +1,17 @@
+//
+//  RemoteConfigManager.swift
+//  Tawazon
+//
+//  Created by mac on 15/03/2022.
+//  Copyright © 2022 Inceptiontech. All rights reserved.
+//
+
+import UIKit
+import FirebaseRemoteConfig
+
+class RemoteConfigManager : NSObject{
+//    var remoteConfig = RemoteConfig.remoteConfig()
+//    var settings = RemoteConfigSettings()
+//    settings.minimumFetchInterval = 0
+//    remoteConfig.configSettings = settings
+}
